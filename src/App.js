0b7803bcd4404890999a1f7e896c3898f04dc559
Patrick 1983 Syntax <Patrick.Naumann2022@gmail.com>
2022-05-17 10:26:24 +0200

    test

---
+++ src/App.js
@@ -0,0 +1,14 @@
+//import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Hello World! </h1>
+      <h2> Bin ich schon drin?</h2>
+      <h1> Das ist meine erste APP</h1>
+    </div>
+  );
+}
+
+export default App;
